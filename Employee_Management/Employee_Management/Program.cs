9fc4b3f8b25c0dcd3647bb21250a6d43d8a29c43
nrp033 <pawarnitin9433@gmail.com>
2022-10-01 23:26:44 +0530

    Adding Part Time For the Day In the Program

--- Employee_Management/Employee_Management/Program.cs
+++ Employee_Management/Employee_Management/Program.cs
@@ -5,15 +5,20 @@
         static void Main(string[] args)
         {
             int IS_FullTime = 1;
+            int IS_PartTime = 2;
             int EmpRatePerHr = 20;
             int EmpWage = 0;
             int EmpWorkingHr = 0;
             Random rd = new Random();
-            int EmpCheck = rd.Next(0, 2);
+            int EmpCheck = rd.Next(0, 3);
 
             if (EmpCheck == IS_FullTime)
             {
                 EmpWorkingHr = 8;
+            }
+            else if (EmpCheck == IS_PartTime)
+            {
+                EmpWorkingHr = 4;
             }
             else
             {
@@ -21,8 +26,10 @@
 
                 Console.WriteLine("Employee is Absent !");
             }
+
             EmpWage = EmpWorkingHr * EmpRatePerHr;
-            Console.WriteLine("Employee Wage For Day = " +EmpWage);
+
+            Console.WriteLine("\n\tEmployee Wage For Day = " +EmpWage);
             Console.ReadKey();
 
         }
