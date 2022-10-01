0c49c3b19248f2776d49fc58d9b8e2f37ddaa090
nrp033 <pawarnitin9433@gmail.com>
2022-10-01 23:51:16 +0530

    Calculating Employee Wage For Month Using For Loop

--- Employee_Management/Employee_Management/Program.cs
+++ Employee_Management/Employee_Management/Program.cs
@@ -7,29 +7,36 @@
             const int IS_FullTime = 1;
             const int IS_PartTime = 2;
             int EmpRatePerHr = 20;
-            int EmpWage = 0;
-            int EmpWorkingHr = 0;
-            Random rd = new Random();
-            int EmpCheck = rd.Next(0, 3);
-            switch (EmpCheck)
-            {
-                case IS_FullTime:
-                    EmpWorkingHr = 8;
-                    break;
-                case IS_PartTime:
-                    EmpWorkingHr = 4;
-                    break;
-                default:
-                    EmpWorkingHr = 0;
-                    Console.WriteLine("\n\tEmployee is Absent !");
-                    break;
-
-            }
+            int WorkingDays = 20;
            
+            int EmpWage = 0;
+            int MonthWage = 0;
+            int EmpWorkingHr = 0;
 
-            EmpWage = EmpWorkingHr * EmpRatePerHr;
+            for (int day = 0; day < WorkingDays; day++)
+            {
+                Random rd = new Random();
+                int EmpCheck = rd.Next(0, 3);
+                switch (EmpCheck)
+                {
+                    case IS_FullTime:
+                        EmpWorkingHr = 8;
+                        break;
+                    case IS_PartTime:
+                        EmpWorkingHr = 4;
+                        break;
+                    default:
+                        EmpWorkingHr = 0;
+                        break;
 
-            Console.WriteLine("\n\tEmployee Wage For Day = " +EmpWage);
+                }
+
+
+                EmpWage = EmpWorkingHr * EmpRatePerHr;
+                MonthWage += EmpWage;
+                Console.WriteLine("\n\tEmployee Wage For Day = " + EmpWage);
+            }
+            Console.WriteLine("\n\tEmployee Wage For Month : " +MonthWage);
             Console.ReadKey();
 
         }
