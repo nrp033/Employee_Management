ea591d4d6c53fc3a9f786f493bff1ccf6fde66b9
nrp033 <pawarnitin9433@gmail.com>
2022-10-01 22:48:50 +0530

    Calculating Employee Wage For Day

--- Employee_Management/Employee_Management/Program.cs
+++ Employee_Management/Employee_Management/Program.cs
@@ -5,17 +5,24 @@
         static void Main(string[] args)
         {
             int IS_FullTime = 1;
+            int EmpRatePerHr = 20;
+            int EmpWage = 0;
+            int EmpWorkingHr = 0;
             Random rd = new Random();
             int EmpCheck = rd.Next(0, 2);
 
             if (EmpCheck == IS_FullTime)
             {
-                Console.WriteLine("\n\n\tEmployee is Present !");
+                EmpWorkingHr = 8;
             }
             else
             {
-                Console.WriteLine("\n\n\tEmployee is Absent !");
+                EmpWorkingHr = 0;
+
+                Console.WriteLine("Employee is Absent !");
             }
+            EmpWage = EmpWorkingHr * EmpRatePerHr;
+            Console.WriteLine("Employee Wage For Day = " +EmpWage);
             Console.ReadKey();
 
         }
