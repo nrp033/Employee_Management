95a3028db1eb04938eb0e764f4ad5c13e51f2ab3
nrp033 <pawarnitin9433@gmail.com>
2022-10-01 22:23:20 +0530

    Checking if Employee is Present or Absent

---
+++ Employee_Management/Employee_Management/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Employee_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FullTime = 1;
+            Random rd = new Random();
+            int EmpCheck = rd.Next(0, 2);
+
+            if (EmpCheck == IS_FullTime)
+            {
+                Console.WriteLine("\n\n\tEmployee is Present !");
+            }
+            else
+            {
+                Console.WriteLine("\n\n\tEmployee is Absent !");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
