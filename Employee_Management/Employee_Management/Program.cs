ef44a4a6983961e6a8df667fcb1162a7de5b258c
nrp033 <pawarnitin9433@gmail.com>
2022-10-02 01:01:13 +0530

    Calculating Employee wages till Condition of tatal working hours of 100 or max days 20 is reaches for a month

--- Employee_Management/Employee_Management/Program.cs
+++ Employee_Management/Employee_Management/Program.cs
@@ -6,37 +6,45 @@
         {
             const int IS_FullTime = 1;
             const int IS_PartTime = 2;
-            int EmpRatePerHr = 20;
-            int WorkingDays = 20;
-           
-            int EmpWage = 0;
-            int MonthWage = 0;
-            int EmpWorkingHr = 0;
 
-            for (int day = 0; day < WorkingDays; day++)
+            const int EmpRatePerHr = 20;  //Employee Rate Per Hours
+            const int MaxWorkingDays = 20;   //Max Working Days In Month
+            const int MaxMonthHrs = 100;  //Max Hours In Month
+
+
+            int TotalMonthWage = 0;  //stores Employee Wage For Month 
+            int EmpWorkingHr = 0;    //Stores Employee Working Hours for Day
+            int EmpWorkingDays = 0; //Working Days in Month
+            int TotalEmpHrs = 0;
+           while(TotalEmpHrs < MaxMonthHrs && EmpWorkingDays < MaxWorkingDays )
             {
+                EmpWorkingDays++; //Increament Working Day by 1 in Month
+
                 Random rd = new Random();
                 int EmpCheck = rd.Next(0, 3);
-                switch (EmpCheck)
+
+                switch (EmpCheck)   //Checking Condition
                 {
                     case IS_FullTime:
                         EmpWorkingHr = 8;
                         break;
+
                     case IS_PartTime:
                         EmpWorkingHr = 4;
                         break;
+
                     default:
                         EmpWorkingHr = 0;
                         break;
 
                 }
 
+                TotalEmpHrs += EmpWorkingHr;   //Calculating Total Employee Working Hours In Month
+                Console.WriteLine("\tDay #: "+EmpWorkingDays+" Employee Working Hours : "+TotalEmpHrs);
+           }
 
-                EmpWage = EmpWorkingHr * EmpRatePerHr;
-                MonthWage += EmpWage;
-                Console.WriteLine("\n\tEmployee Wage For Day = " + EmpWage);
-            }
-            Console.WriteLine("\n\tEmployee Wage For Month : " +MonthWage);
+            TotalMonthWage = MaxMonthHrs * EmpRatePerHr;    //Calculating Emplyee Wage For Month
+            Console.WriteLine("\n\n\t******* Employee Wage For Month : " +TotalMonthWage+" *******");
             Console.ReadKey();
 
         }
